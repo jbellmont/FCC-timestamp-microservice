3c7a55a011f85c4ce13c4596eac40d6f1c7ba4f5
jbellmont <bellmont@gmail.com>
2021-12-16 10:18:55 +0000

    Build API handlers

--- server.js
+++ server.js
@@ -12,12 +12,35 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// API.
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+// API route handlers.
+app.get("/api", function (req, res) {
+  const currentDate = new Date();
+  res.json({ unix: currentDate.getTime(), utc: currentDate.toUTCString() });
 });
 
-// Listener.
+app.get("/api/:date", function (req, res) {
+  let dateParam = req.params.date;
+
+  /* 
+    If date provided is in a number format (e.g. milliseconds), instead of a 
+    string.
+  */
+  if (Number(dateParam)) {
+    dateParam = Number(dateParam);
+  }
+
+  const date = new Date(dateParam);
+
+  if (date.toUTCString() == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  }
+
+  const dateInMilliseconds = date.getTime();
+  const dateInUTCString = date.toUTCString();
+  res.json({ unix: dateInMilliseconds, utc: dateInUTCString });
+});
+
+// Port listener.
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
